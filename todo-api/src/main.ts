1ebb1ae1b774f367d76273b23c437ab687c2ad36
Riley Martinez <riley.m.martinez@gmail.com>
2024-05-04 23:04:58 -0500

    Add Swagger integration

--- todo-api/src/main.ts
+++ todo-api/src/main.ts
@@ -3,10 +3,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppConstants } from './app.constants';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Todo API')
+        .setDescription('The Todo API description')
+        .setVersion('1.0')
+        .addTag('todo')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
 
     const configService = app.get(ConfigService);
     const port = configService.get(AppConstants.PORT);
