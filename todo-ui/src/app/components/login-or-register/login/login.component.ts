3b9aa41e33dbfcca3771d94aa7f49afc0c959d7f
RileyMartinez <riley.m.martinez@gmail.com>
2024-07-04 00:10:03 -0500

    Add todo and todo-list component boilerplate

--- todo-ui/src/app/components/login-or-register/login/login.component.ts
+++ todo-ui/src/app/components/login-or-register/login/login.component.ts
@@ -50,7 +50,6 @@ export class LoginComponent implements OnInit {
             }),
         );
 
-        console.log(this.loginForm?.value);
         this.loadingService.setLoading(false);
     }
 }
