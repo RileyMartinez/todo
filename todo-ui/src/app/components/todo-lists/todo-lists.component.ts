3f89782d3cb36087118135ebec4d6925b7fa86d2
RileyMartinez <riley.m.martinez@gmail.com>
2024-07-07 19:58:41 -0500

    Add todo-lists component boilerplate

---
+++ todo-ui/src/app/components/todo-lists/todo-lists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-lists',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-lists.component.html',
+  styleUrl: './todo-lists.component.scss'
+})
+export class TodoListsComponent {
+
+}
