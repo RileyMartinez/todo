7d3ff1a08ed4f506b388dd163348f690ee6ff551
RileyMartinez <riley.m.martinez@gmail.com>
2024-08-02 16:18:22 -0500

    Remove injection token

--- todo-ui/src/app/injection-tokens/user.token.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../models/user.model';
-
-export const USER_OBSERVABLE_TOKEN = new InjectionToken<Observable<User | null>>('USER_OBSERVABLE_TOKEN');
