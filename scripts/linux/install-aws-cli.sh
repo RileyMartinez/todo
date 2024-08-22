4323b38822f56e90b4d361c16e8a764641629748
RileyMartinez <riley.m.martinez@gmail.com>
2024-08-22 01:14:44 -0500

    Add aws cli install/update script

---
+++ scripts/linux/install-aws-cli.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# See https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+echo "Downloading AWS CLI..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+
+if [ -d "/usr/local/aws-cli" ]; then
+    echo "AWS CLI already installed. Updating..."
+    sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+else
+    echo "Installing AWS CLI..."
+    sudo ./aws/instal
+fi
+
+rm -rf aws awscliv2.zip
