548f20b84ec4e5e070ecabf404792530feac3f6f
RileyMartinez <riley.m.martinez@gmail.com>
2025-01-27 00:25:28 -0600

    Update invalid token cookie configuration

--- api/src/common/configs/invalid-token-cookie.config.ts
+++ api/src/common/configs/invalid-token-cookie.config.ts
@@ -1,18 +1,8 @@
-import { config } from 'dotenv';
-import { AppConstants } from '../constants';
 import { CookieOptions } from 'express';
-
-config();
-
-if (!process.env.ENV) {
-    throw new Error('ENV is not defined');
-}
-
-const isProduction = process.env.ENV === AppConstants.PROD;
 
 export const invalidTokenCookieConfig: CookieOptions = {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'strict',
     maxAge: 0,
 };
