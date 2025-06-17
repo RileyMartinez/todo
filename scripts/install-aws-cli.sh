c2a50b781e3624a9689b4ef27bcb090d68a956fe
Riley Martinez <riley.m.martinez@gmail.com>
2025-06-16 20:07:15 -0500

    Remove obselete scripts

--- scripts/install-aws-cli.sh
+++ scripts/install-aws-cli.sh
@@ -2,6 +2,7 @@
 # See https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 
 echo "Downloading AWS CLI..."
+sudo apt install unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 
--- scripts/win32/install-powershell.ps1
+++
@@ -1,2 +0,0 @@
-# Install latest Powershell release with winget
-winget install --id Microsoft.Powershell --source winget
